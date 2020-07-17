8b98f99cb711101377721d11eca4b883c6b401f2
Orel Hassid <57160714+orelhassid@users.noreply.github.com>
2020-07-17 09:39:28 +0300

    Strapi API url (no env)

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: process.env.API_URL || "http://localhost:1337",
+        apiURL: process.env.API_URL || "https://orelhassid-site.herokuapp.com",
         contentTypes: [
           // List of the Content Types you want to be able to request from Gatsby.
           "article",
